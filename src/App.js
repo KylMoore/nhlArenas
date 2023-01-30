29ef06f8702f57679b4b698bf27946be364dd832
KyleMoore <kylemoore-@hotmail.com>
2023-01-30 17:08:54 -0500

    basic stuff that

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
 import { useState, useEffect } from 'react';
 
+import axios from 'axios';
 import './App.css';
 
+import Form from './components/Form';
 
 
 function App() {
 
-  const [team, setTeam] = useState();
+  const [team, setTeam] = useState("");
+
+  const handleSelectChange = (e) => {
+    console.log(setTeam)
+  }
+
+  useEffect(() => {
+
+    axios({
+      url: "https://statsapi.web.nhl.com/api/v1/teams",
+      method: "GET",
+      dataResponse: "json",
+    }).then((response) => {
+      // setTeam(response.data.teams);
+      console.log(response.data.teams)
+    })
+  })
+
 
 
   return (
     <div className="App">
       <div className='backgroundImage'>
         <h1>NHL Arena</h1>
-        
+        {
+          team.map(name) = ()=>{
+
+            return (
+        <Form
+          team={name.team}
+          handleSelectChange={handleSelectChange}
+        />)
+          }
+        }
       </div>
     </div>
   );
@@ -22,4 +50,3 @@ function App() {
 export default App;
 
 
-// GET https://statsapi.web.nhl.com/api/v1/teams
---
+++ src/components/Form.js
@@ -0,0 +1,44 @@
+const Form = () => {
+    return (
+        <form>
+            <label htmlFor="team">Team: </label>
+            <select>
+                <option>New Jersey Devils</option>
+                <option>New York Islanders</option>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+            </select>
+        </form>
+    )
+}
+
+export default Form
