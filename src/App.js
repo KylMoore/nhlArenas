879df110ac1a2b0cbc30034ce6b2f49b297b7ca6
KyleMoore <kylemoore-@hotmail.com>
2023-01-31 17:20:17 -0500

    some added pseudocode and modifications to components

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import Form from './components/Form';
 
 function App() {
 
-  const [team, setTeam] = useState("");
+  const [team, setTeam] = useState([]);
 
   const handleSelectChange = (e) => {
-    console.log(setTeam)
+    // console.log(setTeam)
   }
 
   useEffect(() => {
@@ -21,27 +21,26 @@ function App() {
       method: "GET",
       dataResponse: "json",
     }).then((response) => {
-      // setTeam(response.data.teams);
-      console.log(response.data.teams)
+      return(
+        
+        console.log(response.data.teams)
+      )
     })
-  })
+  }, [])
 
+const handleSubmit = (userChoice) => {
+  
+}
 
 
   return (
     <div className="App">
       <div className='backgroundImage'>
-        <h1>NHL Arena</h1>
-        {
-          team.map(name) = ()=>{
+        <h1>NHL Arena Tracker</h1>
 
-            return (
         <Form
-          team={name.team}
-          handleSelectChange={handleSelectChange}
-        />)
-          }
-        }
+          handleSubmit={handleSubmit}
+        />
       </div>
     </div>
   );
@@ -49,4 +48,36 @@ function App() {
 
 export default App;
 
+
+// MVP
+
+// on page load mount screen with dropdown menu
+// 	if loading have a loading screen
+// when selecting a team from the drop down change on App.js
+// 	fetch the team data from the api when selecting/setting team on the Form.js
+// 		hook into first render of APP to fetch team info 
+// 	create a peice of state(dropdown menu on Form.js) that reflects the users choice	
+// 		pass handlesubmit function as a prop on APP.js
+// 		mount the team arena or city the team plays in (arena information state on APP.js)
+// 	when changing the selected option 
+// 		create new fetch call to change the information
+
+
+// list of components
+
+// App.js
+// Form.js
+// 	pass handlesubmit function as prop
+
+
+// list of state
+
+// dropdown menu selection(Form.js)
+// Arena Information(App.js)
+
+
+// Stretch
+
+// add a firebase database that can track what teams arenas you've been to with little checkboxes?
+// add in images that load with the team selected to show an arena image, and a location image
 
